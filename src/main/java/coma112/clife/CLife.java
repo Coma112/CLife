76695c9bd208f29397707f599a718b2ba9e3be3c
Coma112 <lolkd005@gmail.com>
2024-07-04 23:53:43 +0200

    Basic Files

---
+++ src/main/java/coma112/clife/CLife.java
@@ -0,0 +1,16 @@
+package coma112.clife;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CLife extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        //Initializing
+    }
+
+    @Override
+    public void onDisable() {
+        //Database disconnect.
+    }
+}
